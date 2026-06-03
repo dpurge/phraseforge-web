4bc72e06adab1e4c34cbfcd9e0e46ceedc739fba
D. Purge <david.purge@gmail.com>
2026-06-03 02:39:10 +0200

    Configure for GitHub pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -35,14 +35,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://phraseforge.example.com',
+  url: 'https://dpurge.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/phraseforge-web/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'phraseforge', // Usually your GitHub org/user name.
+  organizationName: 'dpurge', // Usually your GitHub org/user name.
   projectName: 'phraseforge-web', // Usually your repo name.
 
   onBrokenLinks: 'throw',
